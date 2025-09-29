6094cb087d1866b9974af083afe003a19be63ecd
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2025-09-29 14:40:25 +0530

    first commit

--- roboshop.sh
+++ roboshop.sh
@@ -2,16 +2,39 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0cda10cd2281e68bd" # replace with your id
+ZONE_ID="Z086445618CDQ31LFJNM3" # replace with your id
+DOMAIN_NAME="daws-86s.space" # replace with your domain name
 
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ "$instance" != "frontend" ]; then
-            IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text) 
+            IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+            RECORD_NAME="$instace.$DOMAIN_NAME"
     else
-            IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text) 
+            IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+            RECORD_NAME="$DOMAIN_NAME"
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
