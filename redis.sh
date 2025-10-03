548414b5324cfe1830fee98086cf6cc333c883f8
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2025-10-03 13:13:17 +0530

    first commit

--- redis.sh
+++ redis.sh
@@ -10,7 +10,6 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
-
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
@@ -28,22 +27,20 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-dnf module disable redis -y $>> $LOG_FILE
-VALIDATE $? "Disabling default redis"
-
-dnf module enable redis:7 -y $>> $LOG_FILE
-VALIDATE $? "Enabling redis 7"
-
-dnf install redis -y $>> $LOG_FILE
-VALIDATE $? "Installing redis"
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default Redis"
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Redis 7"
+dnf install redis -y  &>>$LOG_FILE
+VALIDATE $? "Installing Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing Remote connections to Redis"
 
-systemctl enable redis $>> $LOG_FILE
-VALIDATE $? "Enabling redis"
-
-systemctl start redis $>> $LOG_FILE
-VALIDATE $? "Starting redis"
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
